395f641be1b1d1055f71fe80e6260e7759e16505
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2015-12-06 21:43:11 -0500

    ga acceped

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 var FirebaseTokenGenerator = require("firebase-token-generator");
 
-app.use(express.static('assets'));
+app.use(express.static('./'));
 
 var tokenGenerator = new FirebaseTokenGenerator("WbekgyPe55xRsg6GeQwnSM4J4rYk2WXVn80uXDw2");
 var token = tokenGenerator.createToken({uid: "1", some: "arbitrary", data: "here"});
